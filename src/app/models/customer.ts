be961ac4851b1fbd36c6b33fe076c2cd2c66bf3e
unknown <howtobeastupid@gmail.com>
2025-01-07 17:41:41 -0500

    adding a model

---
+++ src/app/models/customer.ts
@@ -0,0 +1,20 @@
+export interface Customer {
+
+    
+        fName: string,
+        lName:string,
+        email:string,
+        number:string,
+        address:string,
+        city:string,
+        province:string,
+        postal:string,
+        date: Date,
+        note:string,
+        message: string,
+        custId: number,
+        service:string,
+        cost: number,
+      
+
+}
